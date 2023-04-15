e1fc1f107eb8961d27a6a754f58aed2debbb79a6
Bekam <mspbekan@gmail.com>
2023-04-15 17:34:15 -0500

    forecast for all days displayed and css code added

--- asset/script.js
+++ asset/script.js
@@ -7,18 +7,9 @@ var searchButton = document.getElementById('search-btn')
 var userCity = document.querySelector('input[name ="city-input"]')
 // var cityNameDiv = $('#city-now') ???why not working
 var cityNameDiv = document.getElementById('city-now')
-
-
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=' + myKey)
-// .then(res => res.json())
-// .then(data => console.log(data))
-
-
-
-// fetch('http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=' + myKey)
-// .then(res => res.json())
-// .then(data => console.log(data))
-
+var forecstAllContainer = document.getElementById('forecast-container')
+var fivedayWrapperDiv = document.getElementById('five-dayWrapperDiv')
+var fiveDayHEl = document.getElementById('fiveDay-heading')
 
 
 searchButton.addEventListener('click', function (event) {
@@ -30,13 +21,9 @@ searchButton.addEventListener('click', function (event) {
 
 })
 
+
+
 //    
-
-
-
-
-
-
 
 function displayForecast() {
 
@@ -53,43 +40,77 @@ function displayForecast() {
             console.log(data[0].name)
             cityNameDiv.appendChild(cityNamePEl)
 
+            cityNamePEl.setAttribute('style', 'font-weight:bold; font-size: large')
+
 
             var lat = data[0].lat
             var lon = data[0].lon
 
-            fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=8&appid=' + myKey + '&units=imperial')
+            fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + myKey + '&units=imperial')
                 .then(res => res.json())
                 .then(data => {
                     console.log(data)
 
-                    for (var i = 0; i < 6; i++) {
+                    var todayEl = document.createElement('span')
+                    var todayIconEl = document.createElement('img')
+                    var todayTempP = document.createElement('p')
+                    var todayWindP = document.createElement('p')
+                    var todayHumidityP = document.createElement('p')
 
-                        var dateEl = document.createElement('span')
+                    todayEl.textContent = ' (' + dayjs(data.list[0].dt_txt).format('M/DD/YYYY') + ')'
+                    todayIconEl.src = 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png';
+                    todayTempP.textContent = 'Temp: ' + data.list[0].main.temp + '°F'
+                    todayWindP.textContent = 'Wind: ' + data.list[0].wind.speed + 'MPH'
+                    todayHumidityP.textContent = 'Humidity: ' + data.list[0].main.humidity + ' %'
+
+                    // var weatherNowDiv = document.createElement('div')
+
+
+                    cityNamePEl.appendChild(todayEl)
+                    cityNameDiv.appendChild(todayIconEl)
+                    cityNameDiv.appendChild(todayTempP)
+                    cityNameDiv.appendChild(todayWindP)
+                    cityNameDiv.appendChild(todayHumidityP)
+
+                    cityNameDiv.classList.add('size-todayDiv')
+
+                    todayIconEl.setAttribute('style', 'width: 30px; height:30px')
+
+
+                    fiveDayHEl.textContent = '5-Day Forecast:'
+                    fiveDayHEl.setAttribute('style', 'font-weight: bold')
+
+                    for (var i = 1; i < data.list.length; i += 8) {
+
+                        var dateEl = document.createElement('p')
                         var iconImgEl = document.createElement('img')
                         var tempP = document.createElement('p')
                         var windP = document.createElement('p')
                         var humidityP = document.createElement('p')
 
-                        dateEl.textContent = dayjs(data.list[i].dt_txt).format('M/DD/YYYY')
-                        // console.log(dayjs(data.list[0].dt_txt).format('M dd yyyy'))
-                        // iconImgEl = data.list[i].weather[i].icon + '.png'
-                        iconImgEl.src = 'http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png';
-                        tempP.textContent = 'temp: ' + data.list[i].main.temp + '°F'
-                        windP.textContent =  'Wind: ' + data.list[i].wind.speed +  'MPH'
-                        humidityP.textContent ='Humidity: ' + data.list[i].main.humidity + ' %'
+                        dateEl.textContent = dayjs(data.list[i + 1].dt_txt).format('M/DD/YYYY')
+                        iconImgEl.src = 'http://openweathermap.org/img/w/' + data.list[i + 1].weather[0].icon + '.png';
+                        tempP.textContent = 'Temp: ' + data.list[i + 1].main.temp + '°F'
+                        windP.textContent = 'Wind: ' + data.list[i + 1].wind.speed + ' MPH'
+                        humidityP.textContent = 'Humidity: ' + data.list[i + 1].main.humidity + ' %'
 
-                        var forecastDiv = document.createElement('div')
-                        var forecstContainer = document.getElementById('forecast-container')
-                        forecstContainer.appendChild(forecastDiv)
-                        forecastDiv.appendChild(dateEl)
-                        forecastDiv.appendChild(iconImgEl)
-                        forecastDiv.appendChild(tempP)
-                        forecastDiv.appendChild(windP)
-                        forecastDiv.appendChild(humidityP)
+                        var eachForecastDiv = document.createElement('div')
+                        fivedayWrapperDiv.appendChild(eachForecastDiv)
 
+                        // fivedayWrapperDiv.setAttribute('style', 'border: solid')
 
+                        // forecstAllContainer.appendChild(eachForecastDiv)
 
-                       
+                        eachForecastDiv.appendChild(dateEl)
+                        eachForecastDiv.appendChild(iconImgEl)
+                        eachForecastDiv.appendChild(tempP)
+                        eachForecastDiv.appendChild(windP)
+                        eachForecastDiv.appendChild(humidityP)
+
+                        eachForecastDiv.setAttribute('style', 'border: solid; margin: 1%;  background-color: gray; color: white; padding: 0 1%; ')
+                        iconImgEl.setAttribute('style', 'width: 30px; height:30px')
+
+                        eachForecastDiv.classList.add('eachForecast-items')
 
                     }
 
@@ -97,19 +118,16 @@ function displayForecast() {
                 })
                 .catch(err => console.log(err))
 
-
-
-
-
         })
         .catch(err => console.log(err))
 
-
+   
 
 }
 
 function saveHistory() {
     console.log('save history function')
+
 
 }
 
