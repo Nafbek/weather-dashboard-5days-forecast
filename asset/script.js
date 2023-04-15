1ad39c216f6b427b1d652c5b145159f37ec4f4e0
Bekam <mspbekan@gmail.com>
2023-04-15 01:23:18 -0500

    data fetched and code to display the forecast data developed

--- asset/script.js
+++ asset/script.js
@@ -1,10 +1,12 @@
 console.log("'I'm connected'")
 
 var myKey = '34e81c1188d1e55fbd1d99bde32f6e18'
+var city;
 
 var searchButton = document.getElementById('search-btn')
-
-
+var userCity = document.querySelector('input[name ="city-input"]')
+// var cityNameDiv = $('#city-now') ???why not working
+var cityNameDiv = document.getElementById('city-now')
 
 
 // fetch('https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=' + myKey)
@@ -19,55 +21,96 @@ var searchButton = document.getElementById('search-btn')
 
 
 
-searchButton.addEventListener('click', function(event){
+searchButton.addEventListener('click', function (event) {
     event.preventDefault()
-    var userCity = document.querySelector ('input[name ="city-input"]')
+
+    //
+    displayForecast()
+    saveHistory()
+
+})
+
+//    
+
+
+
+
+
+
+
+function displayForecast() {
+
+
     console.log(userCity.value)
-    var city =userCity.value;
+    city = userCity.value;
+
+    fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + myKey)  //should i limit the number of cities with same name?
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            var cityNamePEl = document.createElement('p')
+            cityNamePEl.textContent = data[0].name
+            console.log(data[0].name)
+            cityNameDiv.appendChild(cityNamePEl)
 
 
- fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + myKey)
-.then(res => res.json())
-.then(data => {console.log(data)
+            var lat = data[0].lat
+            var lon = data[0].lon
 
-console.log(city.name)
-    var lat = data[0].lat
-    var lon = data[0].lon
+            fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=8&appid=' + myKey + '&units=imperial')
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+
+                    for (var i = 0; i < 6; i++) {
+
+                        var dateEl = document.createElement('span')
+                        var iconImgEl = document.createElement('img')
+                        var tempP = document.createElement('p')
+                        var windP = document.createElement('p')
+                        var humidityP = document.createElement('p')
+
+                        dateEl.textContent = dayjs(data.list[i].dt_txt).format('M/DD/YYYY')
+                        // console.log(dayjs(data.list[0].dt_txt).format('M dd yyyy'))
+                        // iconImgEl = data.list[i].weather[i].icon + '.png'
+                        iconImgEl.src = 'http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png';
+                        tempP.textContent = 'temp: ' + data.list[i].main.temp + '°F'
+                        windP.textContent =  'Wind: ' + data.list[i].wind.speed +  'MPH'
+                        humidityP.textContent ='Humidity: ' + data.list[i].main.humidity + ' %'
+
+                        var forecastDiv = document.createElement('div')
+                        var forecstContainer = document.getElementById('forecast-container')
+                        forecstContainer.appendChild(forecastDiv)
+                        forecastDiv.appendChild(dateEl)
+                        forecastDiv.appendChild(iconImgEl)
+                        forecastDiv.appendChild(tempP)
+                        forecastDiv.appendChild(windP)
+                        forecastDiv.appendChild(humidityP)
 
 
-    // var apiUrl =
-    fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + myKey)
-    .then(res => res.json())
-    .then(data => {console.log(data)
 
-    var par = document.createElement('p')
-    par.textContent = 'temp:' + data.list[0].main.temp
-    var tempEl = document.getElementById('city-now')
-    tempEl.appendChild(par)
+                       
 
-    console.log(par)
-    console.log(data.list[0].weather[0].icon)
-    console.log(data.list[0].main.humidity)
-    console.log(data.list[0].wind.speed)
-    })
+                    }
 
-})
-   
 
-            
-displayForecast()
-saveHistory()
-})
+                })
+                .catch(err => console.log(err))
 
-function displayForecast (){
-   
-    console.log('forecast function') 
-    
+
+
+
+
+        })
+        .catch(err => console.log(err))
+
+
+
 }
 
-function saveHistory(){
+function saveHistory() {
     console.log('save history function')
-   
+
 }
 
 
